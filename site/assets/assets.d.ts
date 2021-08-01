b05c314356cad79e330a649693d3755af1c5a7eb
Bryan McClain <bamcclain10@gmail.com>
2021-08-01 18:14:34 -0400

    Added two.js and various type definitions

---
+++ site/assets/assets.d.ts
@@ -0,0 +1,43 @@
+// Image Files
+declare module '*.png' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.jpg' {
+  const content: string;
+  export default content;
+}
+
+// GLTF Files
+declare module '*.gltf' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.glb' {
+  const content: string;
+  export default content;
+}
+
+// Audio Files
+declare module '*.wav' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.ogg' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.mp3' {
+  const content: string;
+  export default content;
+}
+
+// Text level files
+declare module '*.lvl' {
+  const content: string;
+  export default content;
+}
--- site/src/index.ts
+++ site/src/index.ts
@@ -1,4 +1,24 @@
 import { compute_catacombs } from 'prime-number-catacombs';
+import image from 'assets/catacombs.png';
+import Two from 'two.js';
 import './styles.css';
 
 console.log(compute_catacombs('13333333333'));
+console.log(Two);
+
+const elem = document.getElementById('canvas');
+const two: Two = new Two({ fullscreen: true });
+two.appendTo(elem);
+
+const circle = new Two.Circle(200, 0, 15);
+circle.fill = '#FF8000';
+
+const texture = new Two.Texture(image);
+const sprite = new Two.Sprite(texture);
+sprite.translation.x = 200;
+two.scene.add(sprite);
+two.scene.add(circle);
+
+setInterval(() => {
+  two.update();
+}, 1000);
